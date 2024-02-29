b658e3baabbb86a5a1595ab33f900ed9d96f44dd
BlaineDPattin <89535266+BlaineDPattin@users.noreply.github.com>
2024-02-28 21:26:23 -0600

    Head Bobbing Implemented

--- Assets/Scripts/Player/CameraLook.cs
+++ Assets/Scripts/Player/CameraLook.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraLook : MonoBehaviour
 {
+    [SerializeField] PlayerMovement playerMovement;
+
     [SerializeField] float sensitivityX = 8f;
     [SerializeField] float sensitivityY = 0.5f;
     float mouseX, mouseY;
@@ -12,12 +14,28 @@ public class CameraLook : MonoBehaviour
     [SerializeField] float xClamp = 85f;
     float xRotation = 0f;
 
+    //Head bobbing stuff
+    [SerializeField] float headBobFrequency = 2f;
+    [SerializeField] float headBobAmplitude = 0.1f;
+
+    [SerializeField] float lateralBobFrequency = 5f; // Adjust the frequency of lateral head bobbing while sprinting
+    [SerializeField] float lateralBobAmplitude = 0.05f;
+
+    float headBobOffset = 2f;
+
+    Vector3 originalCameraPosition;
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+
+        originalCameraPosition = playerCamera.localPosition;
     }
     private void Update()
     {
+        HandleHeadBobbing();
+
+
         transform.Rotate(Vector3.up, mouseX * Time.deltaTime);
 
         xRotation -= mouseY;
@@ -31,13 +49,36 @@ public class CameraLook : MonoBehaviour
         mouseX = mouseInput.x * sensitivityX;
         mouseY = mouseInput.y * sensitivityY;
     }
-    
-    
-    
-    
-    
-    
-    
+
+    private void HandleHeadBobbing()
+    {
+       
+
+        // Calculate lateral bobbing motion based on sprinting status
+        if (playerMovement.isGrounded == true)
+        { 
+            float bobAmount = Mathf.Sin(Time.time * headBobFrequency) * headBobAmplitude;
+            float lateralBobAmount = playerMovement.sprint ? Mathf.Sin(Time.time * lateralBobFrequency) * lateralBobAmplitude : 0f;
+            //Calculate the target head bobbing position
+            float targetHeadBobPosition = originalCameraPosition.y + bobAmount;
+
+            // Smoothly interpolate to the target head bobbing position
+            float currentHeadBobPosition = Mathf.Lerp(playerCamera.localPosition.y, targetHeadBobPosition, Time.deltaTime * 5f);
+            // Update the camera's local position with head bobbing
+            playerCamera.localPosition = new Vector3(lateralBobAmount, currentHeadBobPosition, 0f);
+        }
+
+        
+
+        
+    }
+
+
+
+
+
+
+
 
 
 }
