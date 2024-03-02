7d2ccb58b308d4239a4c3e90a3677a1741ffdeb1
Scootic <bobsywoo@gmail.com>
2024-03-02 16:29:43 -0600

    title screen "functional" and "finished"

--- Assets/Scripts/TEMP_TitleLogic.cs
+++ Assets/Scripts/TEMP_TitleLogic.cs
@@ -5,30 +5,48 @@ using UnityEngine.SceneManagement;
 
 public class TEMP_TitleLogic : MonoBehaviour
 {
+    private Animator meAnimator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        meAnimator = this.GetComponent<Animator>();
+        meAnimator.SetBool("showcreds", false);
+        if (meAnimator == null)
+        {
+            Debug.Log("NO ANIMATOR FOR cANVAS!");
+        }
+        else { Debug.Log("animator on canvas :)"); }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     public void LoadAScene(int index)
     {
-        SceneManager.LoadScene(index);
+        if (SceneManager.GetSceneByBuildIndex(index) != null)
+        {
+            Debug.Log("Should be loading scene at index of: " + index);
+            SceneManager.LoadScene(index);
+        }
+        else
+        {
+            Debug.Log("No scene at that index!");
+        }
     }
 
     public void Quit()
     {
+        Debug.Log("Quitting application!");
         Application.Quit();
     }
 
     public void Opencredits()
     {
-        //do something, probably
+        Debug.Log("Should be opening credits");
+        meAnimator.SetBool("showcreds", true);
+    }
+
+    public void Closecredits()
+    {
+        Debug.Log("Should be closing credits");
+        meAnimator.SetBool("showcreds", false);
     }
 }
