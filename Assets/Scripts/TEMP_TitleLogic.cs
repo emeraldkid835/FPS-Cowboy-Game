3dbfa952408c799df82e33965d05742ed294770e
Scootic <bobsywoo@gmail.com>
2024-03-01 22:19:29 -0600

    "campfire"

---
+++ Assets/Scripts/TEMP_TitleLogic.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TEMP_TitleLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadAScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Opencredits()
+    {
+        //do something, probably
+    }
+}
