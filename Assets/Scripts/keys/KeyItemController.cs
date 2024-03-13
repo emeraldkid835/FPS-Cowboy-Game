2ff19cfa47dbedfac8fa4f214fdb95891f569be3
swampshark1 <petegallardo28@gmail.com>
2024-03-13 02:06:47 -0500

    line isnt working
    Assets\Scripts\keys\KeyRaycast.cs(14,17): error CS0246: The type or namespace name 'KeyItemController' could not be found (are you missing a using directive or an assembly reference?)
    
    it 2 am so im losing my mind this scipt should work and if doesnt i will cry

---
+++ Assets/Scripts/keys/KeyItemController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KeySystem
+{
+	public class KeyItemController : MonoBehaviour
+	{
+		[SerializeField] private bool redDoor =false;
+		[SerializeField] private bool redKey = false;
+
+		[SerializeField] private KeyInventory _keyInventory = null;
+
+		private KeyDoorController doorObject;
+
+		private void Start()
+		{
+			if(redDoor)
+			{
+				doorObject = GetCompent<KeyDoorController>();
+			}
+		}
+
+		public void ObjectInteraction()
+		{
+			if (redDoor)
+			{
+				doorObject.PlayAnimation();
+
+			}
+			
+			else if(redKey)
+			{
+				_keyInventory.hasRedKey = true;
+				gameObject.SetActive(false);
+
+			}
+		}
+	}
+ 
+}
