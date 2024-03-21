3e0571669389188b99aa66a486b4760348956f20
Scootic <bobsywoo@gmail.com>
2024-03-21 16:01:41 -0500

    primitive scene loading

---
+++ Assets/Scripts/Event_On_Trigger_Enter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+[RequireComponent(typeof(Collider))]
+public class Event_On_Trigger_Enter : MonoBehaviour
+{
+    [SerializeField] private UnityEvent mevent;
+    [SerializeField] private bool playerOnly = true;
+    private Collider me;
+
+    private void Start()
+    {
+        me = GetComponent<Collider>();
+        me.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(playerOnly == true) { if(other.tag == "Player") { mevent.Invoke(); } } else { mevent.Invoke(); }
+
+    }
+
+}
