257c838730ad2f47c0ab9edbbddce91f61fb092a
Scootic <bobsywoo@gmail.com>
2024-04-28 13:57:27 -0500

    weaponunlocker now cares about prefs

--- Assets/Scripts/Gun/WeaponUnlocker.cs
+++ Assets/Scripts/Gun/WeaponUnlocker.cs
@@ -6,30 +6,39 @@ public class WeaponUnlocker : MonoBehaviour
 {
     // Reference to the WeaponSwitcher script on the player object
     public WeaponSwitcher weaponSwitcher;
-    [SerializeField] int weaponIndex; // int of 1 = shotgun, int of 2 = fireball shooter
+    [SerializeField] int weaponIndex; // int of 1 = shotgun, int of 2 = fireball shooter, 3 = winchester (depends on player prefab)
 
     private void Start()
     {
-        weaponSwitcher = GameObject.Find("GoodPlayer").GetComponentInChildren<WeaponSwitcher>();
+        if (weaponSwitcher == null)
+        {
+            weaponSwitcher = GameObject.Find("GoodPlayer").GetComponentInChildren<WeaponSwitcher>();
+        }
+        if(PlayerPrefs.GetInt("GunUnlocked_" + weaponIndex) == 1)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     // Handle collider trigger events to unlock weapons
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("player in trigger");
-        // Check if the collider belongs to a gun
-        GameObject collidedObject = other.gameObject;
-        if (collidedObject != null)
-        {
-            Debug.Log("Gun is found");
-            // Find the index of the collided gun
-            int index = weaponIndex;
+        
+            Debug.Log("player in trigger");
+            // Check if the collider belongs to a gun. (What?)
+            GameObject collidedObject = other.gameObject;
+            if (collidedObject != null)
+            {
+                Debug.Log("Gun is found");
+                // Find the index of the collided gun
+                int index = weaponIndex;
 
-            // Unlock the gun
-            weaponSwitcher.UnlockWeapon(index);
-            weaponSwitcher.SwitchGun(index);
-        }
+                // Unlock the gun
+                weaponSwitcher.UnlockWeapon(index);
+                weaponSwitcher.SwitchGun(index);
+            }
 
-        Destroy(this.gameObject);
+            Destroy(this.gameObject);
     }
+    
 }
