5ccd7f3a36c7e3ff14b85851bd6f988cb23d40f8
Caleb Shade (Student) <bobsywoo@gmail.com>
2024-04-29 11:23:40 -0500

    endgame for OW!

--- Assets/Scripts/OWSPAGHETTI.cs
+++ Assets/Scripts/OWSPAGHETTI.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class OWSPAGHETTI : MonoBehaviour
 {
+    [SerializeField] private GameObject allEnemies;
+    [SerializeField] private npc mayor;
+    [SerializeField] ParticleSystem piss;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(PlayerPrefs.GetInt("PlayerJump") > 1 && PlayerPrefs.GetInt("GunUnlocked_1") == 1)
+        {
+          
+            mayor.SwapIndex(2);
+            Destroy(allEnemies);
+            piss.Stop();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
