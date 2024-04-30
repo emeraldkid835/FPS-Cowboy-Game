dfc65ce580d1940a447fb16c29e36bac4a6170ac
Scootic <bobsywoo@gmail.com>
2024-04-30 12:31:54 -0500

    add dust, move images, add models, smoke crack

---
+++ Assets/Scripts/spaghetti/OWSPAGHETTI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OWSPAGHETTI : MonoBehaviour
+{
+    [SerializeField] private GameObject allEnemies;
+    [SerializeField] private npc mayor;
+    [SerializeField] ParticleSystem piss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerPrefs.GetInt("PlayerJump") > 1 && PlayerPrefs.GetInt("GunUnlocked_2") == 1)
+        {
+            mayor.SwapIndex(2);
+            Destroy(allEnemies);
+            piss.Stop();
+        }
+    }
+
+}
